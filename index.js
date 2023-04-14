c3717ff051960831ce92191cc5e2b2b0f7f7e398
PrescillaKodehode <113029569+Prescilla2022@users.noreply.github.com>
2023-04-14 23:32:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(cors());
 //app.use(express.urlencoded({ extended: false }));
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "true");
 });
 
 app.use("/register", require("./routes/register"));
