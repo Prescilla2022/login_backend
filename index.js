91b90ea9c1b43ec1f499c14073988008f8b3ec5e
PrescillaKodehode <113029569+Prescilla2022@users.noreply.github.com>
2023-04-15 12:29:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ const cors = require("cors");
 const PORT = process.env.PORT || 10000;
 app.use(express.static(path.resolve(__dirname, "./frontEnd/build")));
 // Step 2:
-app.get("*", function (request, response) {
-  response.sendFile(path.resolve(__dirname, "./frontEnd/build", "index.html"));
+app.get("/",  (req, res) {
+  res.send("server running"));
 });
 
 app.use(express.json());
